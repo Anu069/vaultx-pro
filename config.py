a3e9eabc33e08eaf43bc8d36859b49c486bb706f
Anu069 <117940149+Anu069@users.noreply.github.com>
2026-04-02 19:26:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,10 +7,9 @@ class Config:
     MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.getenv("MAIL_USERNAME")    #( you can your eamil here)
-    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")          # use 16 digit passowrd here
-    MAIL_DEFAULT_SENDER = os.getenv("MAIL_USERNAME")  #( you can your eamil here)
-
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME")   
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")        
+    MAIL_DEFAULT_SENDER = os.getenv("MAIL_USERNAME")  
     # ── Upload Config ─────────────────────────────
     UPLOAD_FOLDER = os.path.join("static", "vault_uploads")
     MAX_CONTENT_LENGTH = 50 * 1024 * 1024  # 50 MB
