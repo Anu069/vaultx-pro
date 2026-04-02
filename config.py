60defc77d2496e7478b7e41eb155def7d8be5ab1
Anu069 <117940149+Anu069@users.noreply.github.com>
2026-04-02 19:25:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os
 
 class Config:
-    SECRET_KEY = os.urandom(24)
+    SECRET_KEY = os.getenv("SECRET_KEY", os.urandom(24))
 
-    # ── Email Config (FILL THESE IN) ─────────────────────────────
-    MAIL_SERVER         = 'smtp.gmail.com'
-    MAIL_PORT           = 587
-    MAIL_USE_TLS        = True
-    MAIL_USERNAME       = 'YOUR_GMAIL_HERE'        # ←  gmail@gmail.com 
-    MAIL_PASSWORD       = 'YOUR_APP_PASSWORD_HERE' # ← 16 digit App Password 
-    MAIL_DEFAULT_SENDER = 'YOUR_GMAIL_HERE'        # ← same gmail 
+    # ── Email Config ─────────────────────────────
+    MAIL_SERVER = "smtp.gmail.com"
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME")    #( you can your eamil here)
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")          # use 16 digit passowrd here
+    MAIL_DEFAULT_SENDER = os.getenv("MAIL_USERNAME")  #( you can your eamil here)
 
-    # ── Upload Config ─────────────────────────────────────────────
-    UPLOAD_FOLDER   = os.path.join('static', 'vault_uploads')
-    MAX_CONTENT_LENGTH = 50 * 1024 * 1024  # 50 MB max file size
+    # ── Upload Config ─────────────────────────────
+    UPLOAD_FOLDER = os.path.join("static", "vault_uploads")
+    MAX_CONTENT_LENGTH = 50 * 1024 * 1024  # 50 MB
